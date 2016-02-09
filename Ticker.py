b356ca9c99cf06e537e2e1e083ddd0421bc0635b
karldiab <karldiab@live.com>
2016-02-08 23:33:43 -0800

    Added main menu and option to add or remove a ticker. Remove functionality still not complete

--- Ticker.py
+++ Ticker.py
@@ -1,5 +1,6 @@
 import urllib.request
 import json
+import sys
 
 STOCKSLIST = "FavoriteStocks.txt"
 
@@ -7,10 +8,11 @@ STOCKSLIST = "FavoriteStocks.txt"
 #doesn't exist, it is created
 def openFavStocks():
     global favStocks
-    print("Loading favorite stocks from file", STOCKSLIST)
     try:
         favStocks = open(STOCKSLIST, 'r+')
-    except (IOError):
+    #
+    except IOError as e:
+        print(e)
         favStocks = open(STOCKSLIST, 'wb')
         favStocks.close()
         favStocks = open(STOCKSLIST, 'r+')
@@ -41,7 +43,6 @@ def fetchStockInfo():
     response = urllib.request.urlopen(url)
     responseString = response.read().decode("utf-8")
     stockInfo = json.loads(responseString)
-    print(stockInfo["results"][0]["name"])
 
 #Ensures each requested ticker is valid. Compares the requested tickers (global var stocks) to what
 #stocks were returned by the info API and removes the stocks not found. Mutates global variable stocks
@@ -66,29 +67,71 @@ def displayStockInfo():
     global stocks
     favStocks.seek(0)
     stocksString = favStocks.read()
-    stocks = stocksString.split(' ')
-    fetchStockInfo()
-    checkForInvalidTicker()
-    for i in stockInfo["results"]:
-        print()
-        print("Symbol:", i["symbol"])
-        print("Name:", i["name"])
-        print("Last Price:", i["lastPrice"])
-        print("Open:", i["open"])
-        print("High:", i["high"])
-        print("Low:", i["low"])
-        print("Close:", i["close"])
-        print("Exchange:", i["exchange"])
+    if len(stocksString) == 0:
+        print("No Stocks entered.")
+    else:
+        stocks = stocksString.split(' ')
+        fetchStockInfo()
+        checkForInvalidTicker()
+        for i in stockInfo["results"]:
+            print()
+            print("Symbol:", i["symbol"])
+            print("Name:", i["name"])
+            print("Last Price:", i["lastPrice"])
+            print("Open:", i["open"])
+            print("High:", i["high"])
+            print("Low:", i["low"])
+            print("Close:", i["close"])
+            print("Exchange:", i["exchange"])
 
 
 #Shows menu user can choose from
-def showMenu():
-    print("Select an option:")
+def getChoice():
+    validOptions = ["1","2","3","4"]
+    print("Options:")
     print("1 - Fetch info for selected tickers")
     print("2 - Add a ticker")
     print("3 - Remove a ticker")
+    print("4 - Quit")
+    option = input("Select an option: ")
+    #input validation
+    while option not in validOptions:
+        errorMsg = "Invalid option. The only valid choices are "
+        for i in validOptions:
+            errorMsg += i + ' '
+        option = input(errorMsg)
+    return option
+def addStock():
+    stockToAdd = input("Enter Stock Symbol: ")
+    #Go to end of faveStocks
+    favStocks.seek(0,2)
+    favStocks.write(' ' + stockToAdd.upper())
+    print(stockToAdd.upper(), "added.")
+    displaySelectedStocks()
+##STILL NOT WORKING
+def removeStock():
+    displaySelectedStocks()
+    stockToRemove = input("Enter Stock to Remove: ")
+    while stockToRemove.upper() not in stocks or stockToRemove.upper() != "QUIT":
+        stockToRemove = input("Stock not in list. Enter a stock in the list or 'quit' to return to main menu.")
+    if stockToRemove.upper() == "QUIT":
+        return 0
 
-openFavStocks()
 
-displayStockInfo()
-closeFavStocks()
+#Runs the main program
+def runTheShow():
+    openFavStocks()
+    while 1:
+        choice = getChoice()
+        if choice == "1":
+            displayStockInfo()
+        elif choice == "2":
+            addStock()
+        elif choice == "3":
+            print()
+            #removeStock()
+        elif choice == "4":
+            break
+    closeFavStocks()
+
+runTheShow()
