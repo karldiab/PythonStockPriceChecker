7140754689fde3602a65d848e2acaccbcba759e0
Karl <karl@karldiab.com>
2016-02-14 17:25:56 -0800

    Finished first version of project. Completed feature to add and remove stocks from external stocks file. Added extra logic to remove extra white space created by removing stocks

--- Ticker.py
+++ Ticker.py
@@ -7,30 +7,43 @@ STOCKSLIST = "FavoriteStocks.txt"
 #Opens external file containing the user's favorite stocks. If the file
 #doesn't exist, it is created
 def openFavStocks():
-    global favStocks
+    global favStocksFile
     try:
-        favStocks = open(STOCKSLIST, 'r+')
-    #
+        favStocksFile = open(STOCKSLIST, 'r+')
     except IOError as e:
         print(e)
-        favStocks = open(STOCKSLIST, 'wb')
-        favStocks.close()
-        favStocks = open(STOCKSLIST, 'r+')
-    displaySelectedStocks()
+        favStocksFile = open(STOCKSLIST, 'wb').close()
+        favStocksFile = open(STOCKSLIST, 'r+')
 
-#Display selected stock tickers
+
+#Display selected stock tickers. Also puts favStockFile contents into global string favStocks
 def displaySelectedStocks():
-    favStocks.seek(0)
-    readStocks = favStocks.read()
-    if len(readStocks) == 0:
-        readStocks = "None"
-    print("Selected Stock Tickers:", readStocks)
+    global favStocks
+    favStocksFile.seek(0)
+    favStocks = favStocksFile.read()
+    if len(favStocks) == 0:
+        print("No stocks entered.")
+    else:
+        #Remove excess white space if present
+        tempStocks = favStocks.split(' ')
+        favStocks = ""
+        for item in tempStocks:
+            if len(item) > 0:
+                favStocks += item + " "
+        #Remove final space
+        favStocks = favStocks[:-1]
+        print("Selected Stock:", favStocks)
 
 #closes external favorite stocks file
 def closeFavStocks():
-    global favStocks
-    favStocks.close()
-
+    favStocksFile.close()
+#Syncs the external stocks file to the favStocks string kept in memoryls
+def updateStocksFile():
+    favStocksFile.close()
+    open(STOCKSLIST, "w").close()
+    openFavStocks()
+    favStocksFile.write(favStocks)
+    
 ##Takes in a list of stock tickers and fetches pricing info about them via JSON API. Then converts the data to
 ##dictionary form as global var stockInfo
 def fetchStockInfo():
@@ -40,6 +53,7 @@ def fetchStockInfo():
         url += item + ','
     #remove extra ',' at the end of url
     url = url[:-1]
+    print("\nFetching data from the internet...")
     response = urllib.request.urlopen(url)
     responseString = response.read().decode("utf-8")
     stockInfo = json.loads(responseString)
@@ -48,8 +62,8 @@ def fetchStockInfo():
 #stocks were returned by the info API and removes the stocks not found. Mutates global variable stocks
 #so it only contains the stock tickers the API found info for. Prints which invalid stocks were removed.
 def checkForInvalidTicker():
-    global stocks
-    global stockInfo
+    global favStocks
+    favStocks = ""
     validStocks = []
     for item in stocks:
         for i in stockInfo["results"]:
@@ -58,19 +72,20 @@ def checkForInvalidTicker():
     for item in stocks:
         if item not in validStocks:
             print(item, "not found. Removing from stocks list.")
-    stocks = validStocks
+        else:
+            favStocks += item + ' '
+    updateStocksFile()
 
 ##Reads user's stocks from the external file and puts them into a list. Fetches info for those
 ##stocks and ensures info was found for every requested stock. Prints the information for each
-##requested stock
+##requested stock.
 def displayStockInfo():
     global stocks
-    favStocks.seek(0)
-    stocksString = favStocks.read()
-    if len(stocksString) == 0:
+    if len(favStocks) == 0:
         print("No Stocks entered.")
+        stocks = []
     else:
-        stocks = stocksString.split(' ')
+        stocks = favStocks.split(' ')
         fetchStockInfo()
         checkForInvalidTicker()
         for i in stockInfo["results"]:
@@ -83,11 +98,12 @@ def displayStockInfo():
             print("Low:", i["low"])
             print("Close:", i["close"])
             print("Exchange:", i["exchange"])
+    print()
 
 
 #Shows menu user can choose from
 def getChoice():
-    validOptions = ["1","2","3","4"]
+    validOptions = ("1","2","3","4")
     print("Options:")
     print("1 - Fetch info for selected tickers")
     print("2 - Add a ticker")
@@ -101,35 +117,45 @@ def getChoice():
             errorMsg += i + ' '
         option = input(errorMsg)
     return option
+#Adds a stock to end of file. Does no validation
 def addStock():
-    stockToAdd = input("Enter Stock Symbol: ")
+    stockToAdd = input("\nEnter Stock Symbol: ")
     #Go to end of faveStocks
-    favStocks.seek(0,2)
-    favStocks.write(' ' + stockToAdd.upper())
+    favStocksFile.seek(0,2)
+    favStocksFile.write(' ' + stockToAdd.upper())
     print(stockToAdd.upper(), "added.")
+#Removes stockToRemove from file.
+#Precondition: stockToRemove must be in the favStocks string
+def removeStock(stockToRemove):
+    global favStocks
+    favStocks = favStocks.replace(stockToRemove,"")
+    print(stockToRemove, "removed.")
+    updateStocksFile()
+#Prompts the user for which stock they want removed. Ensures the entered stock
+#is actually in favStocks string
+def removeStocksPrompt():
+    print("\nCurrently entered stocks:")
     displaySelectedStocks()
-##STILL NOT WORKING
-def removeStock():
-    displaySelectedStocks()
-    stockToRemove = input("Enter Stock to Remove: ")
-    while stockToRemove.upper() not in stocks or stockToRemove.upper() != "QUIT":
-        stockToRemove = input("Stock not in list. Enter a stock in the list or 'quit' to return to main menu.")
+    stockToRemove = input("Enter stock to remove or 'quit' to return to main menu: ")
+    while stockToRemove.upper() not in favStocks and stockToRemove.upper() != "QUIT":
+        stockToRemove = input("Stock not in list. Enter a stock in the list or 'quit' to return to main menu: ")
     if stockToRemove.upper() == "QUIT":
         return 0
+    removeStock(stockToRemove.upper())
 
 
 #Runs the main program
 def runTheShow():
     openFavStocks()
     while 1:
+        displaySelectedStocks()
         choice = getChoice()
         if choice == "1":
             displayStockInfo()
         elif choice == "2":
             addStock()
         elif choice == "3":
-            print()
-            #removeStock()
+            removeStocksPrompt()
         elif choice == "4":
             break
     closeFavStocks()
